c479324a505d63056e5ef84cdeabf8eb05cfc8d1
hieu <phuchieuct93@gmail.com>
2018-12-27 20:39:37 +0700

    Update responsive

--- client/src/app/shared/breakpoint.service.ts
+++ client/src/app/shared/breakpoint.service.ts
@@ -9,9 +9,8 @@ export class BreakpointDetectorService {
     public isSmallScreen: boolean;
 
     constructor(private breakpointObserver: BreakpointObserver) {
-        const breakpointStateObservable = this.breakpointObserver.observe([Breakpoints.Small,Breakpoints.XSmall]).subscribe(value => {
+        const breakpointStateObservable = this.breakpointObserver.observe(["(max-width: 575px)"]).subscribe(value => {
             this.isSmallScreen = value.matches;
-            console.log(this.isSmallScreen)
             breakpointStateObservable.unsubscribe();
         })
     }

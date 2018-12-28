f41302d257279f678c1f9ddab0bd01c9e9c5a1bd
hieu <phuchieuct93@gmail.com>
2018-12-28 19:17:41 +0700

    Update container

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {Config, ConfigService} from "./shared/config.service";
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
     name = 'Angular';

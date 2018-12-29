f9daf231d9a459811e3f6dc4000f944aa99faa4f
hieu <phuchieuct93@gmail.com>
2018-12-29 15:25:45 +0700

    Update loading

--- client/src/app/article/loading/loading.component.ts
+++ client/src/app/article/loading/loading.component.ts
@@ -9,34 +9,7 @@ const msgs: LoadingMessage[] = [
     {
         msg: "Đợi tí nhe, tớ đang kéo về",
         icon: 'search'
-    },
-    {
-        msg: "Axon đang tuyển dụng, apply lẹ nào",
-        icon: 'donut_large'
-    },
-    {
-        msg: "Hãy thay đổi thế giới",
-        icon: 'face'
-    },
-    {
-        msg: "Đọc báo để nâng cao kiến thức",
-        icon: 'language'
-    },
-    {
-        msg: "Đọc báo để có gấu",
-        icon: 'favorite_border'
-    },
-    {
-        msg: "Tôi sẽ biến mất nhanh thôi",
-        icon: 'directions_run'
-    },
-    {
-        msg: "Đừng mong chờ phải nhìn thấy tôi",
-        icon: 'fiber_manual_record'
-    },  {
-        msg: "Tôi yêu bạn",
-        icon: 'favorite_border'
-    },
+    }
 ]
 
 @Component({

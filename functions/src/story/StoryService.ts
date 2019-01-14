c12ad8199cfa7b1db846ed1033c29e3d23ab7898
hieu <phuchieuct93@gmail.com>
2019-01-14 20:09:35 +0700

    Increase cache number

--- functions/src/story/StoryService.ts
+++ functions/src/story/StoryService.ts
@@ -5,7 +5,7 @@ import ArticleServiceFactory from "../article/ArticleServiceFactory";
 
 
 export abstract class StoryService {
-    readonly MAX_CACHE_NUMBER: 30;
+    readonly MAX_CACHE_NUMBER: 50;
     protected storyParser: StoryParser;
 
     abstract getStories(pageNumber: string, category: string): Promise<Story[]>;
@@ -25,7 +25,7 @@ export abstract class StoryService {
 
     private cacheArticles(stories): Promise<any> {
         let promise = this.cacheArticle(stories[0].id);
-        for (let i = 1; i < 30; i++) {
+        for (let i = 1; i < this.MAX_CACHE_NUMBER; i++) {
             promise = (function (story: Story) {
                 return promise.then((value) => {
                     if (value) {

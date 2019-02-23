8fba947db9d0632bdcbe062aa68f43bf76aa938c
hieu <phuchieuct93@gmail.com>
2019-02-23 19:58:02 +0700

    Update category

--- client/src/app/navigator/category-selector/category/category.component.ts
+++ client/src/app/navigator/category-selector/category/category.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Category} from "../../../../../../model/Categories";
 
 @Component({
     selector: 'app-category',
@@ -8,7 +9,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class CategoryComponent implements OnInit {
 
     @Input()
-    category: CategoryComponent
+    category: Category
 
     constructor() {
     }

06458908fdfeda40990eb6b370b56100fbbeb7b2
hieu <phuchieuct93@gmail.com>
2019-04-05 21:25:57 +0700

    Update animation

--- client/src/app/article/loading/loading.component.ts
+++ client/src/app/article/loading/loading.component.ts
@@ -2,31 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {ConfigService} from "../../shared/config.service";
 import {BreakpointDetectorService} from "../../shared/breakpoint.service";
 
-interface LoadingMessage {
-    msg: string,
-    icon: string
-}
-
-const msgs: LoadingMessage[] = [
-    {
-        msg: "Đợi tí nhe, tớ đang kéo về",
-        icon: 'search'
-    }
-]
-
 @Component({
     selector: 'app-loading',
     templateUrl: './loading.component.html',
     styleUrls: ['./loading.component.scss']
 })
 export class LoadingComponent implements OnInit {
-    msg: LoadingMessage;
 
-    constructor(public config: ConfigService,public breakpointService:BreakpointDetectorService) {
+    constructor(public config: ConfigService, public breakpointService: BreakpointDetectorService) {
     }
 
     ngOnInit() {
-        this.msg = msgs[Math.floor(Math.random() * msgs.length)];
     }
 
 }

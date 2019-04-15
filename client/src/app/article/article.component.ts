2aa5d3bf49a55ca6d0c5259fc36e580491432b70
hieu <phuchieuct93@gmail.com>
2019-04-15 17:24:13 +0700

    Revert

--- client/src/app/article/article.component.ts
+++ client/src/app/article/article.component.ts
@@ -7,11 +7,27 @@ import ArticleContentParser from "./article-parser";
 import {DomService} from "./dom.service";
 import {ConfigService} from "../shared/config.service";
 import {Subscription} from "rxjs";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-article',
     templateUrl: './article.component.html',
     styleUrls: ['./article.component.scss'],
+    animations: [
+        trigger('showArticle', [
+                transition(':enter', [
+                    style({opacity: 0}),
+                    animate('0.5s', style({opacity: 1})),
+                ]),
+                transition(':leave', [
+                    style({opacity: 1}),
+                    animate('0.5s', style({
+                        opacity: 0
+                    }))
+                ])
+            ]
+        )
+    ],
 
 })
 export class ArticleComponent implements OnInit, OnDestroy {

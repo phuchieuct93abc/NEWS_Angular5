4ae37a72290a62643c9ef986115cddce6f95ae37
hieu <phuchieuct93@gmail.com>
2019-06-16 22:07:53 +0700

    Update sitemap

--- functions/copy-index.js
+++ functions/copy-index.js
@@ -42,3 +42,10 @@ fs.copy(robot[0], robot[1], function (err) {
         return console.error(err)
     }
 });
+var robot = ['../client/dist/browser/sitemap.xmk', './dist/browser/sitemap.xml']
+
+fs.copy(robot[0], robot[1], function (err) {
+    if (err) {
+        return console.error(err)
+    }
+});

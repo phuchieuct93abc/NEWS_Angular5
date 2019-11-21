5f13f84fd66c218bd1a30ec7bc259c7efc97e7b3
hieu <phuchieuct93@gmail.com>
2019-11-21 20:44:35 +0700

    Update max-width for content

--- client/src/app/article/display/display.component.ts
+++ client/src/app/article/display/display.component.ts
@@ -7,21 +7,24 @@ import {BreakpointDetectorService} from "../../shared/breakpoint.service";
     selector: 'app-display',
     templateUrl: './display.component.html',
     styleUrls: ['./display.component.scss'],
-    encapsulation:ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None
 })
 export class DisplayComponent implements OnInit {
 
     isDarkMode: boolean;
     isSmallImage: boolean;
     fontSize: number;
+    minFontSize = ConfigService.MIN_FONTSIZE;
+    maxFontSize = ConfigService.MAX_FONTSIZE;
+
 
     constructor(private route: ActivatedRoute,
                 private configService: ConfigService,
-                public breakpointService: BreakpointDetectorService) {
+                public breakpointService: BreakpointDetectorService,) {
     }
 
     ngOnInit() {
-        this.isDarkMode = this.configService.getConfig().darkTheme
+        this.isDarkMode = this.configService.getConfig().darkTheme;
         this.isSmallImage = this.configService.getConfig().smallImage;
         this.fontSize = this.configService.getConfig().fontSize
 
@@ -29,13 +32,13 @@ export class DisplayComponent implements OnInit {
 
 
     toggleDarkMode(value) {
-        console.log(this.isDarkMode)
+        console.log(this.isDarkMode);
         this.configService.updateConfig({darkTheme: value})
     }
 
     changeFontSize(value: number) {
-        console.log(value)
-        this.configService.updateConfig({fontSize: value})
+        console.log(value);
+        this.configService.updateConfig({fontSize: value});
         this.fontSize = value
 
     }

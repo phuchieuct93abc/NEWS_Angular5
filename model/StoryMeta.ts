a09de1ba08a14260f2270594bc77ba94ce96d49a
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2020-10-31 20:03:48 +0700

    Update timeago for baomoi

--- model/StoryMeta.ts
+++ model/StoryMeta.ts
@@ -3,13 +3,11 @@ import moment from "../functions/utils/moment.service";
 export default class StoryMeta {
     public source: string;
     public time: string | number;
-    public timeago: string
     public sourceIcon: string
 
     constructor(source: string, sourceIcon: string, time: string | number) {
         this.source = source;
         this.time = time;
-        this.timeago = moment.timeago(this.time);
         this.sourceIcon = sourceIcon;
     }
 }

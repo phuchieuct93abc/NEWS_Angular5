48426d215392fed757a80df54ae6e1bb42da20be
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2020-11-22 15:13:10 +0700

    update image can not be loaded

--- functions/src/article/baomoi/BaomoiArticleParser.ts
+++ functions/src/article/baomoi/BaomoiArticleParser.ts
@@ -1,66 +1,72 @@
-import {ArticleParser} from "../ArticleParser";
+import { ArticleParser } from "../ArticleParser";
 import Article from "../../../../model/Article";
 import Utility from "../../Utility";
-import BaomoiStoryParser from '../../story/baomoi/BaomoiStoryParser';
+import BaomoiStoryParser from "../../story/baomoi/BaomoiStoryParser";
 
 export default class BaomoiArticleParser extends ArticleParser {
-
     constructor() {
         super();
     }
 
-    private convertHtmlBody(images:string[]):string {
-
-        let body = this.data.getElementsByClassName('article__body')[0].innerHTML;
-        body =  Utility.replaceAll(body, "data-", "");
-        return this.addThumbnail(images,body)
+    private convertHtmlBody(): string {
+        let dataHTML = this.sanityLazyImages();
+        return dataHTML.innerHTML;
     }
 
-    private addThumbnail(images:string[], body:string){
-        if(images.length>0){
-            let thumbnail = images[0];
-            if(body.indexOf(thumbnail)===-1){
-                body = `<img class="thumbnail-inner" src='${thumbnail}'/> ${body} `
-            } 
+    private sanityLazyImages() {
+        let dataHTML = this.data.getElementsByClassName('article__body')[0] as HTMLElement;
+        let lazyImages = dataHTML.getElementsByClassName("lazy-img");
+        for (let i = 0; i < lazyImages.length; i++) {
+            lazyImages[i].setAttribute("src", lazyImages[i].getAttribute("data-src"));
         }
-        return body;
-
+        return dataHTML;
     }
-
 
     parserArticle(): Article {
-        const header = this.data.getElementsByClassName('article__header')[0].textContent;
-        const id = this.data.getElementsByClassName('article')[0].getAttribute('data-aid');
-        const sourceName = this.data.getElementsByClassName('source')[0].textContent;
+        const header = this.data.getElementsByClassName("article__header")[0].textContent;
+        const id = this.data.getElementsByClassName("article")[0].getAttribute("data-aid");
+        const sourceName = this.data.getElementsByClassName("source")[0].textContent;
         let sourceUrl = this.data.querySelector(".article__action .plsh").getAttribute("href");
         sourceUrl = `https://m.baomoi.com${sourceUrl}`;
 
-        let images:string[] = this.extractImages();
-        const description = this.data.getElementsByClassName('article__sapo')[0].textContent;
+        let images: string[] = this.extractImages();
+        const description = this.data.getElementsByClassName("article__sapo")[0].textContent;
 
         let likes = parseInt(this.data.querySelector(".like").textContent);
-        let time = this.data.querySelector("time.time").getAttribute('datetime');
+        let time = this.data.querySelector("time.time").getAttribute("datetime");
 
-        let sourceIconUrl = BaomoiStoryParser.getSourceIconUrl(this.data.querySelector('.article__meta'))
+        let sourceIconUrl = BaomoiStoryParser.getSourceIconUrl(this.data.querySelector(".article__meta"));
 
-        return new Article(id, header, null, this.convertHtmlBody(images), null, null, null, sourceUrl, sourceName, sourceIconUrl,images, description, likes, time, this.extractRalatedNumber());
+        return new Article(
+            id,
+            header,
+            null,
+            this.convertHtmlBody(),
+            null,
+            null,
+            null,
+            sourceUrl,
+            sourceName,
+            sourceIconUrl,
+            images,
+            description,
+            likes,
+            time,
+            this.extractRalatedNumber()
+        );
     }
-
-
     private extractImages(): string[] {
         let images = [];
-        const imageElements = this.data.getElementsByTagName('img');
+        const imageElements = this.data.getElementsByTagName("img");
         for (let index = 0; index < imageElements.length; index++) {
-            images.push(imageElements[index].getAttribute('src'))
+            images.push(imageElements[index].getAttribute("src"));
         }
-
 
         return images;
     }
 
     private extractRalatedNumber(): number {
         let articleMeta = this.data.querySelector(".article__meta");
-        return articleMeta.childElementCount === 3 ? parseInt(articleMeta.lastElementChild.textContent) : 0
-
+        return articleMeta.childElementCount === 3 ? parseInt(articleMeta.lastElementChild.textContent) : 0;
     }
 }

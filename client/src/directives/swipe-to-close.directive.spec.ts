665811c4855635429ed7236c56519a9974b8910a
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-01-31 14:40:31 +0700

    refactor code

--- client/src/directives/swipe-to-close.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SwipeToCloseDirective } from './swipe-to-close.directive';
-
-describe('SwipeToCloseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SwipeToCloseDirective(null);
-    expect(directive).toBeTruthy();
-  });
-});

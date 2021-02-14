196a6946cfc486aa3c1461a797811d39232f5404
hieu <phuchieuct93@gmail.com>
2021-02-14 22:29:03 +0700

    Update environment.server.ts

--- client/src/environments/environment.server.ts
+++ client/src/environments/environment.server.ts
@@ -1,5 +1,5 @@
 const CONFIG = {
-    baseUrl: 'https://us-central1-angularhero-3b066.cloudfunctions.net/apissr/', 
+    baseUrl: 'https://asia-northeast1-angularhero-3b066.cloudfunctions.net/api/', 
     production: true,
 };
 

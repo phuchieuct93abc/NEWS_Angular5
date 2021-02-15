50dac32c62a8af89aad2cec179a75c3c458d9197
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-02-15 20:42:51 +0700

    interceptor

--- client/src/app/interceptor.ts
+++ client/src/app/interceptor.ts
@@ -12,7 +12,7 @@ export class NoopInterceptor implements HttpInterceptor {
 
     public intercept(req: HttpRequest<any>, next: HttpHandler):
         Observable<HttpEvent<any>> {
-        console.time(req.url);
-        return next.handle(req).pipe(tap(()=>console.timeLog(req.url)));
+        console.time(req.url+req.params);
+        return next.handle(req).pipe(tap(()=>console.timeLog(req.url+req.params)));
     }
 }

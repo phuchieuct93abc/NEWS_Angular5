af749eab1ba133e0f28bc93931cab92c73f22a46
hieu <phuchieuct93@gmail.com>
2021-02-16 09:16:18 +0700

    Update server.ts

--- functions/server.ts
+++ functions/server.ts
@@ -4,17 +4,17 @@ import app from "./app";
 
 
 exports.app = functions.runWith({
-    timeoutSeconds: 30,
+    timeoutSeconds: 20,
     memory: '2GB'
 }).https.onRequest(app);
 
 exports.apissr = functions.runWith({
-    timeoutSeconds: 30,
+    timeoutSeconds: 10,
     memory: '1GB'
 }).https.onRequest(api);
 
 exports.api = functions.runWith({
-    timeoutSeconds: 300,
+    timeoutSeconds: 10,
     memory: '1GB'
 }).region("asia-northeast1").https.onRequest(api);
 

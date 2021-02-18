3b2ecc155be9cd3fc1abce9d18f143e0dba7f975
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-02-18 15:21:43 +0700

    render image ssr

--- client/src/app/story/image-viewer/image-viewer.component.ts
+++ client/src/app/story/image-viewer/image-viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Inject, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
 import { Subject } from 'rxjs';
 import { IS_MOBILE } from 'src/app/shared/const';
 import { ImageSerice } from '../../shared/image.service';
@@ -23,6 +24,8 @@ export class ImageViewerComponent implements OnInit, OnDestroy {
     public parallax: boolean;
     @Input()
     public maxParallax = 40;
+    // @Input()
+    // public responsive = false;
 
 
     public onDestroy$ = new Subject<void>();
@@ -36,7 +39,8 @@ export class ImageViewerComponent implements OnInit, OnDestroy {
     public constructor(private imageService: ImageSerice,
         private elRef: ElementRef<HTMLElement>,
         @Inject(IS_MOBILE) private isMobile: boolean,
-        @Inject(IS_NODE) private isNode: boolean) {
+        @Inject(IS_NODE) private isNode: boolean,
+        private transferState: TransferState) {
     }
 
     public ngOnInit() {
@@ -51,13 +55,27 @@ export class ImageViewerComponent implements OnInit, OnDestroy {
     private refreshImageResolution() {
 
         if (this.imagePath) {
+            const imageKey = makeStateKey<{imagePath:string,width:Number,height:number}>(`imageviewer-${this.imagePath}`);
+
+            if(this.transferState.hasKey(imageKey)){
+                const {imagePath,width,height}=this.transferState.get(imageKey, null);
+                this.convertedImagePath = imagePath;
+                this.height = height;
+                this.width = width;
+                return
+            }
            const resolution =  new RegExp(/w\d*_r(\d*)x(\d*)/gm).exec(this.imagePath);
             if(resolution){
                 this.width = parseInt(resolution[1],10);
                 this.height = parseInt(resolution[2],10);
             }
             if(this.isNode){
-                this.convertedImagePath = this.imagePath;
+                this.convertedImagePath = this.imagePath+'.webp';
+                this.transferState.set(imageKey,{
+                    imagePath: this.convertedImagePath,
+                    width: this.width,
+                    height: this.height
+                })
 
             }else{
                 setTimeout(() => {

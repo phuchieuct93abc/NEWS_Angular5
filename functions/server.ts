93fe981136b8439250aceca6fd4b2eab18af97ae
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-02-19 18:29:18 +0700

    update function angular

--- functions/server.ts
+++ functions/server.ts
@@ -3,19 +3,28 @@ import api from "./api";
 import app from "./app";
 
 
-exports.app = functions.runWith({
-    timeoutSeconds: 20,
-    memory: '1GB'
-}).https.onRequest(app);
+const function_name = process.env.FUNCTION_NAME || process.env.K_SERVICE;
+if (!function_name || function_name === 'app') {
+    exports.app = functions.runWith({
+        timeoutSeconds: 20,
+        memory: '1GB'
+    }).https.onRequest(app);}
+if (!function_name || function_name === 'apissr') {
+    exports.apissr = functions.runWith({
+        timeoutSeconds: 10,
+        memory: '1GB'
+    }).https.onRequest(api);}
+if (!function_name || function_name === 'api') {
+    exports.api = functions.runWith({
+        timeoutSeconds: 10,
+        memory: '1GB'
+    }).region("asia-northeast1").https.onRequest(api);}
 
-exports.apissr = functions.runWith({
-    timeoutSeconds: 10,
-    memory: '1GB'
-}).https.onRequest(api);
 
-exports.api = functions.runWith({
-    timeoutSeconds: 10,
-    memory: '1GB'
-}).region("asia-northeast1").https.onRequest(api);
+
+
+
+
+
 
 

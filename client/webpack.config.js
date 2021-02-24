01f95e1d049f8ee7bf3dac07ef8420559d6a6ecc
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-02-24 21:55:49 +0700

    try build with postbuild.js

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          syntax: 'postcss-scss',
+          plugins: () => [
+            require('postcss-import'),
+            require('autoprefixer'),
+            purgecss({
+              content: ['./**/*.html'],
+              // Example to let PurgeCss know how to exclude cdk and mat prefixes if your using Angular CDK and Angular Material
+              whitelistPatterns: [/^cdk-|mat-/],
+              defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            })
+          ]
+        }
+      }
+    ]
+  }
+};

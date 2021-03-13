d04f42e8c4efc8c94a9845c594c2ca8092a1eec7
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-03-13 17:36:47 +0700

    fix error

--- client/src/app/shared/checkForUpdate.service.ts
+++ client/src/app/shared/checkForUpdate.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { interval } from 'rxjs';
-import environment from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'

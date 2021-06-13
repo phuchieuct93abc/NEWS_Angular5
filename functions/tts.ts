484dcdabb1f0792725c0eb9255cfa1797de8fb13
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-06-13 09:45:47 +0700

    preload

--- functions/tts.ts
+++ functions/tts.ts
@@ -10,50 +10,39 @@ const util = require('util');
 const client = new textToSpeech.TextToSpeechClient();
 const jsdom = require("jsdom");
 
-const {JSDOM} = jsdom;
+const { JSDOM } = jsdom;
 
-const sanitize = (text: string): string=>{
+const sanitize = (text: string): string => {
   const dom = new JSDOM(`<!DOCTYPE html><body>${text}</body>`);
   dom.window.document.querySelector("a")?.remove();
   dom.window.document.querySelector("img")?.remove();
   dom.window.document.querySelector("script")?.remove();
 
 
- let escapedLines = dom.window.document.querySelector("body").textContent.trim();;
- escapedLines = escapedLines.replace(/&/g, '&amp;');
- escapedLines = escapedLines.replace(/"/g, '&quot;');
- escapedLines = escapedLines.replace(/</g, '&lt;');
- escapedLines = escapedLines.replace(/>/g, '&gt;');
- return escapedLines;
+  let escapedLines = dom.window.document.querySelector("body").textContent.trim();;
+  escapedLines = escapedLines.replace(/&/g, '&amp;');
+  escapedLines = escapedLines.replace(/"/g, '&quot;');
+  escapedLines = escapedLines.replace(/</g, '&lt;');
+  escapedLines = escapedLines.replace(/>/g, '&gt;');
+  return escapedLines;
 
 }
 export async function ttsArticle(article: Article) {
-  let text = [article.header, article.description , sanitize(article.body)].join("\n");
+  let text = [article.header, article.description, sanitize(article.body)].join("\n");
   text = text.replace(/\n/g, '\n<break time=\"1\" />');
-  text = `<speak>${text.substring(0,4000)}</speak>`;
-  
+  text = `<speak>${text.substring(0, 4000)}</speak>`;
 
-  // Convert plaintext to SSML
-  // Tag SSML so that there is a 2 second pause between each address
 
-    // Construct the request
   const request = {
-    input: {ssml: text},
-    // Select the language and SSML voice gender (optional)
+    input: { ssml: text },
     voice: {
-      languageCode: 'vi-VN', 
-      name:'vi-VN-Standard-C',
+      languageCode: 'vi-VN',
+      name: 'vi-VN-Standard-C',
     },
-    // select the type of audio encoding
-    audioConfig: {audioEncoding: 'MP3'},
+    audioConfig: { audioEncoding: 'MP3' },
   };
 
-    
-  
-  
-
-  // Performs the text-to-speech request
   const [response] = await client.synthesizeSpeech(request);
 
-return response.audioContent;
+  return response.audioContent;
 }

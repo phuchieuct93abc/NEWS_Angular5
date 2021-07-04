e6009ee5cb0c80dfee60a6d72e8e1f08be4b4cfd
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-07-04 15:44:22 +0700

    remove diffbot

--- functions/src/article/ArticleServiceFactory.ts
+++ functions/src/article/ArticleServiceFactory.ts
@@ -5,7 +5,6 @@ import TinhteArticleService from "./tinhte/TinhteArticleService";
 
 export default class ArticleServiceFactory {
     public static get(category: string): ArticleService {
-        let englishCategory = CategoryHelper.englishCategories().find(cate => cate.name == category);
         if(category==="tinh-te"){
             return new TinhteArticleService();
         }

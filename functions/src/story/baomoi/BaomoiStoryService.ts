22d7effadc2a5175238f8d87d8f676198b6f4213
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-07-04 16:17:06 +0700

    Update version

--- functions/src/story/baomoi/BaomoiStoryService.ts
+++ functions/src/story/baomoi/BaomoiStoryService.ts
@@ -9,7 +9,7 @@ import { createHmac } from 'crypto';
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const axios = require('axios');
-const baomoiStoryparams = "https://baomoi.com/api/v1/content/get/list-by-type?listType=3&listId={categoryId}&page={page}&ctime=1624697699&version=0.1.7&sig={sig}&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw"
+const baomoiStoryparams = "https://baomoi.com/api/v1/content/get/list-by-type?listType=3&listId={categoryId}&page={page}&ctime={ctime}&version=0.1.30&sig={sig}&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw"
 
 export default class BaomoiStoryService extends StoryService {
 
@@ -22,14 +22,16 @@ export default class BaomoiStoryService extends StoryService {
     }
 
     static createInstance(pageNumber: string, category: string) {
-
+        const ctime=Math.floor(new Date().getTime()/1000) +'';
         const categoryId = CategoryHelper.getCategory(category).id + '';
-        const baseParamSign = `/api/v1/content/get/list-by-typectime=1624697699listId=${categoryId}listType=3page=${pageNumber}version=0.1.7`;
+        const baseParamSign = `/api/v1/content/get/list-by-typectime=${ctime}listId=${categoryId}listType=3page=${pageNumber}version=0.1.30`;
         const sig = createHmac('sha256', '882QcNXV4tUZbvAsjmFOHqNC1LpcBRKW').update(baseParamSign).digest("hex");
         let url = baomoiStoryparams
             .replace("{page}", pageNumber)
             .replace("{categoryId}", categoryId)
-            .replace("{sig}", sig);
+            .replace("{sig}", sig)
+            .replace("{ctime}", ctime);
+            console.log(url)
         return new BaomoiStoryService(url, category);
 
     }

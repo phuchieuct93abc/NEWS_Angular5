09e953aff53366f50c92a8c5d9b79746114009ae
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-07-04 15:13:33 +0700

    remove diffbot

--- functions/src/article/ArticleServiceFactory.ts
+++ functions/src/article/ArticleServiceFactory.ts
@@ -1,5 +1,4 @@
 import {ArticleService} from "./ArticleService";
-import GoogleArticleService from "./google-news/GoogleArticleService";
 import BaomoiArticleService from "./baomoi/BaomoiArticleService";
 import CategoryHelper from "../../../model/Categories";
 import TinhteArticleService from "./tinhte/TinhteArticleService";
@@ -9,10 +8,6 @@ export default class ArticleServiceFactory {
         let englishCategory = CategoryHelper.englishCategories().find(cate => cate.name == category);
         if(category==="tinh-te"){
             return new TinhteArticleService();
-        }
-        if (englishCategory!==undefined) {
-
-            return new GoogleArticleService();
         }
         return new BaomoiArticleService(category);
     }
--- functions/src/story/StoryServiceFactory.ts
+++ functions/src/story/StoryServiceFactory.ts
@@ -1,6 +1,5 @@
 import BaomoiStoryService from "./baomoi/BaomoiStoryService";
-import GoogleStoryService from "./google-news/GoogleStoryService";
-import {StoryService} from "./StoryService";
+import { StoryService } from "./StoryService";
 import CategoryHelper from "../../../model/Categories";
 import TinhteStoryService from "./tinhte/TinhteStoryService";
 
@@ -8,17 +7,10 @@ export default class StoryServiceFactory {
     public static get(req): StoryService {
 
         let category = req.query.category;
-        let englishCategory = CategoryHelper.englishCategories().find(cate => cate.name == category);
 
-        if(category=="tinh-te"){
+        if (category == "tinh-te") {
             return TinhteStoryService.createInstance(req.query.pageNumber);
         }
-        if (englishCategory!==undefined) {
-
-            return GoogleStoryService.createInstance(req.query.pageNumber, req.query.category);
-        } else {
-            return BaomoiStoryService.createInstance(req.query.pageNumber, req.query.category);
-        }
-        return null
+        return BaomoiStoryService.createInstance(req.query.pageNumber, req.query.category);
     }
 }

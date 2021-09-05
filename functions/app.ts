9c956ebd6252447472f48b434d7d2b415297ad6b
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-09-05 23:06:44 +0700

    postinstall

--- functions/app.ts
+++ functions/app.ts
@@ -2,14 +2,10 @@
 import * as express from 'express';
 import {join} from 'path';
 
-// Faster server renders w/ Prod mode (dev mode never needed)
-// Express server
-const expressApp = express();
 
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 const {app} = require(`./dist/server/main`);
 
-app();
 
-export default expressApp;
+
+export default app();

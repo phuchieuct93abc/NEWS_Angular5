9ca16bce9ca5004aabf31b6c9463c5d45beefdbc
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-09-06 21:36:17 +0700

    refactor code

--- functions/src/notification/notificationHandler.ts
+++ functions/src/notification/notificationHandler.ts
@@ -2,34 +2,21 @@ import NotificationService from "./notification.service";
 import ArticleServiceFactory from "../article/ArticleServiceFactory";
 
 
-export default function notifyHandler(notify) {
+export default async function notifyHandler(notify) {
 
     let notificationService = new NotificationService();
-    notify.get('/notify', (req, response) => {
-
+    notify.get('/notify', async (req, response) => {
         let articleId = req.query.articleId;
-        console.log(articleId)
-        ArticleServiceFactory.get(req.query.category).getArticleById(articleId).then(article => {
-
-            notificationService.send(article,req.query.category).then(result => {
-                response.send(result);
-            });
-
-        })
-
-
+        const article = await ArticleServiceFactory.get(req.query.category).getArticleById(articleId);
+        const result = await notificationService.send(article, req.query.category);
+        response.send(result);
     });
 
-
-    notify.post('/subscribe', (req, response) => {
+    notify.post('/subscribe', async (req, response) => {
         let topic = req.body.topic;
         let token = req.body.token;
-        notificationService.subscribeTopic(token, topic).then((result) => {
-            response.send(result)
-        });
-
-
+        const result = await notificationService.subscribeTopic(token, topic);
+        response.send(result)
     })
-
 
 };

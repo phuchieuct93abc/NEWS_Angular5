31545d2cf5f626bc924648bc52f19dfd4f0b561e
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-09-06 21:10:02 +0700

    update firebase api

--- model/firebase.config.ts
+++ model/firebase.config.ts
@@ -1,11 +1,12 @@
-export const FIREBASE_CONFIG = {
+export const FIREBASE_CONFIG  = {
     apiKey: "AIzaSyCLs6d7wzgF1nmbc8rIfkbCmEnnoD8yvt8",
     authDomain: "angularhero-3b066.firebaseapp.com",
     databaseURL: "https://angularhero-3b066.firebaseio.com",
     projectId: "angularhero-3b066",
     storageBucket: "angularhero-3b066.appspot.com",
-    messagingSenderId: "17159897246"
-}
+    messagingSenderId: "17159897246",
+    appId: "1:17159897246:web:018f1d36c3d98641ee3b8f"
+  };
 export const firebaseCredentiaL = {
     "projectId": "angularhero-3b066",
     "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDPqV1d03fE3OwD\nuGb/xmBG04tOogLaaq0mLluneRVFBo3s7bYWOUOa5Pm7NLt8hikQEAax+OtdNEye\ng1hVjgqFbh0aV8qQybg4+FgmX87XdLSkWZI9NPOe4hshnZzYP/066J+EZJw5JY3H\nYa1H6osLAwYXg5zQ9yarMcFlOkhTZ3ao6eyNIXlAC9t8ztXbRTqlqn34ywkyGlCi\nIK0H7oMAfBVKpidkeWyFUS7RczYJWI1QDb0O7QNEPJ4FhFkkEduUwfrPxPcE3rMi\nync2CRwOhXUilcHlwqcsKYeuA+H8nQlmbGggn6wdXQN0ISauoqf0iZKUiyI51scm\ndTAou0BpAgMBAAECggEAYJwJKWD72/TxwktUc9M9MEYzjboWvLmbY2N21YOh7xJ9\nDciLiWxLtPCo9Dy0qtqZmPTzwHGvQhXWPXqaqnbiEzBmzr4gKdsG304+aF/zUU5k\nyf8350zOuFC10Z5LfVnLvE/EAqpx+a4sdyFMpeQKmu1ltKPQ3XDWJxgHQspqIo5t\nW0qdB85fGnVMYFIB43eFcq+qwuWGsSLiVHv9GFJjt0ounSlqHbSyWm3eGrWKz5qv\n+PXXH+2c4gUZhzX3skVOma1I3X1jUoQFw1KONssUIAlQ/D7kzVZkacudVLZDoUkn\nbmRgKXj2UqDswTr27JKCZTBd8zvZ9Ngc5PivlpawXwKBgQD3IAD5AD3sQ5Jev4+e\n2MGQDs6O7S8nmKhDMimV6vA+bSHtXz/X4/iKMfD2JJO0SHDdgJuvwxTgckjlPneK\nvKJuZSLAYHfH4EDWW33VLAsYbcQoTHfS/MFJWG7YIIJGeDKgtWMXqlihFy5Zg29A\nwJNK4QDnAsAmql9mvVxcKuJ9DwKBgQDXHouj5R9gF6vNvetsAwtGuLh2wkoq2hJK\n52EeVwMUIrmAVZAJujiu+yZIPuJS2sYuOFeslLKARBicVRUaRXSMona7fhTEPeHs\nyZSaEWdcBPYr8osxv0t/9OLZOrrwYOngRZCf/Tn+IGofmdZTCSfRc7dCoZvra8gE\nZgtTh5LbBwKBgQC9194na2ImoHpPQ0Xnt4hrTJSuA9OXzZRlmqGLw+IvctL/cY6w\nITeLonYEAXFeq8/YubXgPjNJgUIyqdxXvjXiLNuXggRCjioBAmQWYTFc4OYyNxxN\n/1HVMJRmgALUc/4gq3emdHyFtDMtHOVYjxgYC+YPkXNfwL5tRMcxHtjDTQKBgEmm\nlZyph5J4yEOKsJ2sdSHjgJR4dLgzLWwjx+aBV/Z+5RABO215KosORc8aaJaBfLS6\naZoW1v5/UgXEQ5NZEExOG2SaCDYgEcfmRFBzgPXKZRYq1mlT21+V0k0ZhINkSu6U\nIq4EHuTY6DyliPGpAs79VVMkzaNOpY6L+XjBxA/lAoGAYIKw/o9Di0KCYzHjzqGq\n6whveQfIBT+Eqg2BSrl1v4AAMBbfkmTXeke9rTJxhATHEx12vJX7g7Td73fwKccH\nh1bTPT2NY87+Z6/IAUYGWcQU45oz5/+F4U100CCYudvsfQKMWFlnM8VfIoczjsgw\n00SDncId1V2MSP3o4XoKeak=\n-----END PRIVATE KEY-----\n",

7ffdcabcc3649eece51766756b24171adabc0132
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-09-12 15:35:13 +0700

    parallel build

--- news/src/environments/environment.prod.ts
+++ news/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const CONFIG = {
-    baseUrl: '/api/',
+    baseUrl: 'https://news.phuchieu.cf/api/',
     baseUrlStory: 'https://asia-northeast1-angularhero-3b066.cloudfunctions.net/api/api/',
     production: true
 
--- news/src/environments/environment.ts
+++ news/src/environments/environment.ts
@@ -1,6 +1,6 @@
 const CONFIG = {
-    baseUrl: 'http://192.168.1.115:3001/',
-    baseUrlStory: 'http://192.168.1.115:3001/',
+    baseUrl: 'http://192.168.1.115:3001/api',
+    baseUrlStory: 'http://192.168.1.115:3001/api',
     production: false
 };
 

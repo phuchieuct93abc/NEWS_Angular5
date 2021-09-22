d18f8fc9a5768c3e5575ebefbbf73c6b9f07b6ef
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2021-09-22 09:02:28 +0700

    add todo attachment as video

--- functions/src/article/tinhte/TinhteArticleParser.ts
+++ functions/src/article/tinhte/TinhteArticleParser.ts
@@ -26,6 +26,25 @@ export default class TinhteArticleParser extends ArticleParser<TinhteData> {
     addAttachToBody() {
         let body = this.data.first_post.post_body_html;
         let attachImage = this.data.first_post.attachments; 
+        //TOTO attachment as video:
+        // {
+        //     attachment_id: 5179699,
+        //     attachment_download_count: 0,
+        //     filename: 'video.mp4',
+        //     links: {
+        //       permalink: 'https://photo2.tinhte.vn/data/attachment-files/2021/09/5649702_video.mp4',
+        //       data: 'https://photo2.tinhte.vn/data/attachment-files/2021/09/5649702_video.mp4',
+        //       thumbnail: 'https://9932704de5fc5c8.cmccloud.com.vn/videos/2021/09/5649702/gif/5649702_4d8c874d4f7f3c91ba008ca8257ce5ba.gif',
+        //       video_url: 'https://9932704de5fc5c8.cmccloud.com.vn/videos/2021/09/5649702/540p/b9331e307f3c48bf8db9cd52c3ebaea3.mp4',
+        //       post: 'https://tinhte.vn/appforo/index.php?posts/61001488/'
+        //     },
+        //     attachment_is_video: true,
+        //     attachment_video_is_processing: false,
+        //     video_ratio: 0.56,
+        //     post_id: 61001488,
+        //     permissions: { view: true, delete: false },
+        //     attachment_is_inserted: false
+        //   }
         if (attachImage?.length > 0) {
             let thumbnail = attachImage[0].links.data;
             if(body.indexOf(thumbnail)===-1){

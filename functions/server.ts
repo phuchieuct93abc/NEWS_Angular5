a6c624174ac49d9853a4ff762b1b295c7d26a8d4
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-05-14 15:34:05 +0700

    test

--- functions/server.ts
+++ functions/server.ts
@@ -1,13 +1,12 @@
 import * as functions from 'firebase-functions';
 import router from './api';
-import app from './app';
 
-exports.app = functions
-  .runWith({
-    timeoutSeconds: 20,
-    memory: '2GB',
-  })
-  .https.onRequest(app);
+// exports.app = functions
+//   .runWith({
+//     timeoutSeconds: 20,
+//     memory: '2GB',
+//   })
+//   .https.onRequest(app);
 
 exports.apissr = functions
   .runWith({

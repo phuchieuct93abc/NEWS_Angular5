663cca3373f8d95bcacdbbdf5b3289959994a68d
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-06-04 17:17:31 +0700

    fixed navigator

--- news/src/app/article/actions/actions.component.ts
+++ news/src/app/article/actions/actions.component.ts
@@ -31,7 +31,11 @@ import { IS_NODE } from './../../shared/const';
     trigger('animate', [
       transition(':enter', [
         style({ transform: 'translateY(50%)', opacity: 0 }),
-        animate('500ms', style({ transform: 'translateY(0)', opacity: 1 })),
+        animate('200ms', style({ transform: 'translateY(0)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0)', opacity: 1 }),
+        animate('200ms', style({ transform: 'translateY(50%)', opacity: 0 })),
       ]),
     ]),
   ],

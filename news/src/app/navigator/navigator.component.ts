bb207c0f407374886bf2629f3af5a555fc8c6aef
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-06-04 16:37:50 +0700

    box-shadow for navigator

--- news/src/app/navigator/navigator.component.ts
+++ news/src/app/navigator/navigator.component.ts
@@ -1,14 +1,13 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { ScrollDispatcher } from '@angular/cdk/scrolling';
+import { Component, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { asyncScheduler, BehaviorSubject, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, throttleTime } from 'rxjs/operators';
 import { IS_MOBILE } from 'src/app/shared/const';
 import CategoryHelper from '../../../../model/Categories';
 import { AppComponent } from '../app.component';
 import { AppService } from '../app.service';
-import { ConfigService } from '../shared/config.service';
-import { configFeature, updateConfigAction } from '../store/config.reducer';
-import { Category } from './../../../../model/Categories';
+import { configFeature } from '../store/config.reducer';
 import { DestroySubscriber } from './../shared/destroy-subscriber';
 
 @Component({
@@ -16,39 +15,37 @@ import { DestroySubscriber } from './../shared/destroy-subscriber';
   templateUrl: './navigator.component.html',
   styleUrls: ['./navigator.component.scss'],
 })
-export class NavigatorComponent extends DestroySubscriber implements OnInit {
+export class NavigatorComponent extends DestroySubscriber {
   @ViewChild(AppComponent)
   public app: AppComponent;
   public readonly MIN_TOP = -63;
   public readonly MAX_TOP = 0;
 
-  public toolbarTop = 0;
-
   public isDarkMode: boolean;
   public isSmallImage: boolean;
-  public selectedCategory$: Observable<Category>;
+
+  public selectedCategory$ = this.store.select(configFeature.selectCategory).pipe(
+    map((category) => CategoryHelper.getCategory(category)),
+    debounceTime(100)
+  );
+
+  scroll$ = new BehaviorSubject<number>(0);
+
+  onScrollTop$ = this.scroll$.pipe(
+    throttleTime(100, asyncScheduler, { leading: true, trailing: true }),
+    map(() => window.scrollY === 0),
+    distinctUntilChanged()
+  );
 
   public constructor(@Inject(IS_MOBILE) public isMobile: boolean, private appService: AppService, private store: Store) {
     super();
   }
 
-  public ngOnInit() {
-    this.selectedCategory$ = this.store.select(configFeature.selectCategory).pipe(map((category) => CategoryHelper.getCategory(category)));
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    this.scroll$.next(window.scrollY);
   }
-
-  public restrictTop(top: number): number {
-    return Math.min(this.MAX_TOP, Math.max(this.MIN_TOP, top));
-  }
-
-  public toggleDarkMode() {
-    this.store.dispatch(updateConfigAction({ darkTheme: this.isDarkMode }));
-  }
-
-  public toogleDisplay() {
-    this.store.dispatch(updateConfigAction({ smallImage: this.isSmallImage }));
-  }
-
-  public toogleSidebar() {
+  public toggleSidebar(): void {
     this.appService.toggleSidebar();
   }
 }

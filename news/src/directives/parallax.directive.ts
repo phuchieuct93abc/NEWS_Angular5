c4370882f687eebae902622c820a5de6f8e3775c
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-06-19 14:50:35 +0700

    improve-init-threasthold-parallax

--- news/src/directives/parallax.directive.ts
+++ news/src/directives/parallax.directive.ts
@@ -25,7 +25,6 @@ export class ParallaxDirective implements OnDestroy {
   ) {}
 
   private static initThresholdSet(): number[] {
-    console.log('init');
     const result: number[] = [];
     for (let i = 0; i <= 1.0; i += 0.00001) {
       result.push(i);

841e609d7744a64931ce600c6a24197526ef426b
hieu <phuchieuct93@gmail.com>
2022-06-25 05:01:54 +0000

    upgrade angular 14

--- news/src/app/app-routing.module.ts
+++ news/src/app/app-routing.module.ts
@@ -28,7 +28,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabled',
+      initialNavigation: 'enabledBlocking',
       paramsInheritanceStrategy: 'always',
       relativeLinkResolution: 'legacy',
     }),

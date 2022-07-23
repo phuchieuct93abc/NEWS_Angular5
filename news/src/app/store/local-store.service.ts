fc1e8396531993e8ffa120f7412e64379c5de1c0
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-07-23 14:33:23 +0700

    read article

--- news/src/app/store/local-store.service.ts
+++ news/src/app/store/local-store.service.ts
@@ -1,39 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { LocalStorage, StorageMap } from '@ngx-pwa/local-storage';
-import { filter, switchMap, take } from 'rxjs';
+import { concatMap, filter, merge, mergeMap, Observable, switchMap, take, tap } from 'rxjs';
 import { LocalStorageService } from '../shared/storage.service';
 import { ArticleHistoryData, articleHistoryFeature, loadArticleHistorySuccess } from './article-history.feature';
 import { GoogleLogin, loginFeature, loginSuccess } from './login.effect';
 
 @Injectable()
 export class LocalStoreService {
-  constructor(private store: Store, private localStorageService: LocalStorageService) {}
+  private loadArticleHistory: Observable<unknown>;
+  private storeLoggedUser: Observable<unknown>;
+  private storeArticleHistory: Observable<unknown>;
+  constructor(private store: Store, private localStorageService: LocalStorageService) {
+    this.loadArticleHistory = this.localStorageService
+      .getItem('articleHistory', null)
+      .pipe(tap((articleHistory) => articleHistory && this.store.dispatch(loadArticleHistorySuccess(articleHistory as ArticleHistoryData))));
+
+    this.storeLoggedUser = this.store.select(loginFeature.selectLoginFeatureState).pipe(
+      filter((a) => !!a),
+      tap((user) => {
+        this.localStorageService.setItemSync('loggedUser', user);
+      })
+    );
+    this.storeArticleHistory = this.store.select(articleHistoryFeature.selectArticleHistoryState).pipe(
+      filter((a) => !!a),
+      switchMap((articleHistory) => this.localStorageService.setItem('articleHistory', articleHistory))
+    );
+  }
 
   load(): void {
-    this.store
-      .select(loginFeature.selectLoginFeatureState)
-      .pipe()
-      .subscribe((login) => this.localStorageService.setItemSync('loggedUser', login));
-    this.store
-      .select(articleHistoryFeature.selectArticleHistoryState)
-      .pipe(switchMap((articleHistory) => this.localStorageService.setItem('articleHistory', articleHistory)))
-      .subscribe();
-
-    this.localStorageService
-      .getItem('loggedUser', null)
-      .pipe(
-        filter((a) => !!a),
-        take(1)
-      )
-      .subscribe((loggedUser) => this.store.dispatch(loginSuccess(loggedUser as GoogleLogin)));
-    this.localStorageService
-      .getItem('articleHistory', null)
-      .pipe(
-        filter((a) => !!a),
-        take(1)
-      )
-
-      .subscribe((articleHistory) => this.store.dispatch(loadArticleHistorySuccess(articleHistory as ArticleHistoryData)));
+    const loggedUser = this.localStorageService.getItemSync('loggedUser', null) as GoogleLogin;
+    if (loggedUser) {
+      this.store.dispatch(loginSuccess(loggedUser));
+    }
+    this.storeLoggedUser.subscribe();
+    this.loadArticleHistory.pipe(switchMap(() => this.storeArticleHistory)).subscribe();
   }
 }

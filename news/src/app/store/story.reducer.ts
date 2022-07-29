6bc4199ecf7649532f69ccc65ddf1ebc9fa10420
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-07-29 20:48:27 +0700

    categoryState

--- news/src/app/store/story.reducer.ts
+++ news/src/app/store/story.reducer.ts
@@ -1,14 +1,32 @@
+import { Inject, Injectable } from '@angular/core';
+import { Actions, ofType } from '@ngrx/effects';
 import { createAction, createFeature, createReducer, on, props } from '@ngrx/store';
+import { mergeMap } from 'rxjs';
 import { Story } from '../../../../model/Story';
+import { StoryService } from '../shared/story.service';
 
 export interface LoadedStories {
-  [category: string]: {
-    [id: string]: Story;
+  currentPageNumber: number;
+  currentPayload: string;
+  category: string;
+  stories: Story[];
+  storyMap: {
+    [category: string]: {
+      [id: string]: Story;
+    };
   };
 }
 
 export const addStoryAction = createAction('[Stories] Add Story', props<{ category: string; story: Story }>());
-const initialState: LoadedStories = {};
+export const addStoriesAction = createAction('[Stories] Add Stories', props<{ category: string; story: Story[] }>());
+export const loadMoreStory = createAction('[Stories] Load more', props<{ category: string; story: Story }>());
+const initialState: LoadedStories = {
+  currentPageNumber: 1,
+  currentPayload: null,
+  category: null,
+  stories: [],
+  storyMap: {},
+};
 
 export const loadedStoriesFeature = createFeature({
   name: 'loadedStories',
@@ -21,3 +39,15 @@ export const loadedStoriesFeature = createFeature({
     })
   ),
 });
+@Injectable({
+  providedIn: 'root',
+})
+export class StoreEffect {
+  loadMoreStories$ = this.action.pipe(
+    ofType(loadMoreStory),
+    mergeMap(() => {
+      return this.storyService.getStoryByPage();
+    })
+  );
+  constructor(private action: Actions, private storyService: StoryService) {}
+}

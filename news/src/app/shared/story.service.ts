23b2177a6aa20bf0f64dba4640bcd54f2fc50113
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-07-30 15:59:59 +0700

    fix category

--- news/src/app/shared/story.service.ts
+++ news/src/app/shared/story.service.ts
@@ -14,11 +14,6 @@ const storyUrl = environment.asiaUrl + `story`;
 })
 export class StoryService {
   public onSearch = new Subject<string>();
-  private currentStoryPage = 0;
-  private currentPayload: unknown;
-
-  private stories: Story[] = [];
-  private storiesQueue: Story[] = [];
 
   public constructor(private httpClient: HttpClient, private loadingService: LoadingService, private store: Store) {}
 
@@ -43,48 +38,5 @@ export class StoryService {
         ),
         map((result) => ({ payload: result.payload, story: result.story.map((r) => Object.assign(new Story(), r)) }))
       );
-  }
-
-  public resetPageNumber(): void {
-    this.currentStoryPage = 1;
-    this.stories = [];
-    this.storiesQueue = [];
-  }
-
-  // public getStories(category: string, numberOfStories: number = 5): Observable<Story[]> {
-  //   if (this.storiesQueue.length > 0) {
-  //     return of(this.storiesQueue.splice(0, numberOfStories));
-  //   }
-
-  //   return this.getStoryByPage(category, this.currentStoryPage, this.currentPayload).pipe(
-  //     map((stories: Story[]) => {
-  //       const result = this.filterStory(stories);
-  //       this.appendStoryList(result);
-  //       this.storiesQueue = result;
-  //       this.currentStoryPage++;
-  //       return result;
-  //     }),
-  //     map(() => this.storiesQueue.splice(0, numberOfStories))
-  //   );
-  // }
-
-  // public getStoriesFirstPage(category: string): Observable<Story[]> {
-  //   return this.getStoryByPage(category, 1, null);
-  // }
-
-  public getById(id: string): Story {
-    return this.stories.find((s) => s.id === id);
-  }
-
-  public unshift(firstStory: Story): void {
-    this.stories.unshift(firstStory);
-  }
-
-  private filterStory(stories: Story[]) {
-    return stories.filter((result) => this.stories.findIndex((story) => story.id === result.id) === -1);
-  }
-
-  private appendStoryList(moreStories: Story[]) {
-    this.stories.push(...moreStories);
   }
 }

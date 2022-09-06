e3986cd8eb28f4e697e9e00e883af61869ff2695
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-09-06 21:53:24 +0700

    synchronize category

--- news/src/app/store/reducer.ts
+++ news/src/app/store/reducer.ts
@@ -8,14 +8,19 @@ export function createRehydrateReducer<S, A extends Action = Action>(
   initialState: S,
   ...ons: ReducerTypes<S, ActionCreator[]>[]
 ): ActionReducer<S, A> {
-  const item = localStorage.getItem(key);
+  let item: string;
+  if (typeof localStorage !== 'undefined') {
+    item = localStorage.getItem(key);
+  }
   const newInitialState = (item && JSON.parse(item)) ?? initialState;
 
   const newOns: ReducerTypes<S, ActionCreator[]>[] = [];
   ons.forEach((oldOn: ReducerTypes<S, ActionCreator[]>) => {
     const newReducer: ActionReducer<S, A> = (state: S | undefined, action: ActionType<ActionCreator[][number]>) => {
       const newState = oldOn.reducer(state, action);
-      localStorage.setItem(key, JSON.stringify(newState));
+      if (typeof localStorage !== 'undefined') {
+        localStorage.setItem(key, JSON.stringify(newState));
+      }
       return newState;
     };
     newOns.push({ ...oldOn, reducer: newReducer });

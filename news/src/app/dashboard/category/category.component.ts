d54584d2e55ea9747af7c120463045fd9a16645c
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2022-11-26 17:33:52 +0700

    dequeue

--- news/src/app/dashboard/category/category.component.ts
+++ news/src/app/dashboard/category/category.component.ts
@@ -1,13 +1,40 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { finalize, map, take } from 'rxjs/operators';
 import { Category } from '../../../../../model/Categories';
 import { Story } from '../../../../../model/Story';
 import { opacityNgIf } from '../../animation';
 import { StoryService } from '../../shared/story.service';
 
-let queue = 0;
+class Queue {
+  queue: (() => void)[] = [];
+  checkAndRun$ = new Subject();
+  running = false;
+
+  constructor(private delay = 0) {
+    this.checkAndRun$.subscribe(() => this.checkAndRun());
+  }
+
+  scheduler(action: () => void) {
+    this.queue.push(action);
+    this.checkAndRun();
+  }
+  dequeue() {
+    setTimeout(() => {
+      this.running = false;
+      this.checkAndRun();
+    }, this.delay);
+  }
+  private checkAndRun() {
+    if (!this.running) {
+      this.queue.shift()();
+      this.running = true;
+    }
+  }
+}
+
+const queue = new Queue(500);
 @Component({
   selector: 'app-top-category',
   templateUrl: './category.component.html',
@@ -42,12 +69,13 @@ export class TopCategoryComponent implements OnInit {
   public constructor(private storyService: StoryService) {}
 
   public ngOnInit(): void {
-    setTimeout(() => {
-      this.stories$ = this.storyService
-        .getStoryByPage(this.category?.name, 1, null)
-        .pipe(map(({ story }) => [...story].slice(0, this.maximumStories)));
-    }, queue * 1000);
-    queue++;
+    queue.scheduler(() => {
+      this.stories$ = this.storyService.getStoryByPage(this.category?.name, 1, null).pipe(
+        map(({ story }) => [...story].slice(0, this.maximumStories)),
+        take(1),
+        finalize(() => queue.dequeue())
+      );
+    });
   }
 
   public toggleExpand(): void {

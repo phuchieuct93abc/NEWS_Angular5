730aa5eb4c7824f7f5d5ad48b882b65b3f54e7e8
[Lightning] Lam Phuc Hieu <hieu.lam@axonactive.com>
2023-05-04 22:24:35 +0700

    new style skeleton

--- news/src/app/article/loading/loading.component.ts
+++ news/src/app/article/loading/loading.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, Observable } from 'rxjs';
-import { configFeature } from 'src/app/store/config.reducer';
+import { map, Observable, shareReplay } from 'rxjs';
+import { Theme, configFeature } from 'src/app/store/config.reducer';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
 })
-export class LoadingComponent implements OnInit {
-  public isDarkTheme$: Observable<boolean>;
+export class LoadingComponent {
+  THEME = Theme;
+  public isDarkTheme$ = this.store.select(configFeature.selectTheme);
+  public skeletonTheme$ = this.isDarkTheme$.pipe(map((darkTheme) => (darkTheme ? 'progress-dark' : 'progress')));
+
+  @Input()
+  public mode: 'small' | 'big' = 'small';
   public constructor(private store: Store) {}
-  ngOnInit(): void {
-    this.isDarkTheme$ = this.store.select(configFeature.selectTheme).pipe(map(() => true));
-  }
 }
